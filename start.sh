bc231a5c299ae73cc5e8fffacbd84a20e507cbca
WizardBots <164856167+WizardBots@users.noreply.github.com>
2024-03-25 14:24:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
-cd boted;
-pip3 install -U -r requirements.txt;
-echo "Bot Started....";
-python3 bot.py;
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/WizarBots/File-Shareing-Bot/File-Shareing-Bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /File-Shareing-Bot
+fi
+cd /File-Shareing-Bot
+pip3 install -U -r requirements.txt
+echo "Bot Started...."
+python3 bot.py
